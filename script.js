ac7004ea3e44a8e1d115bd34c2ed618b422fc1cb
magician58 <ishowraryal555@gmail.com>
2024-12-25 19:19:24 +0545

    Its order

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("continueBtn").onclick = function() {
+    window.location.href = "https://youtu.be/LeVTfD6U5jA?si=CsE1-OeuaAIj15Ww";  // Replace with your desired YouTube link
+}
